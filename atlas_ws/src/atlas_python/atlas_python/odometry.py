bfcca02e8fe870e27af7858913da3a8972bcb7f1
Hung Anh Dau <hdau0002@student.monash.edu>
2024-08-20 12:59:46 +1000

    Odometry.py fixed

--- atlas_ws/src/atlas_python/atlas_python/odometry.py
+++ atlas_ws/src/atlas_python/atlas_python/odometry.py
@@ -1,20 +1,15 @@
-# WIP DO NOT USE
-
 # RUN THIS AS A BACKGROUND PROCESS
 
 # ROS 2 Set ups
 import sys
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Float32MultiArray
-
 
 try:
     import RPi.GPIO as GPIO
 except RuntimeError: # RPi.GPIO throws errors when not on RPi
     from unittest.mock import MagicMock as GPIO
 
-import RPi.GPIO
 import time
 import os
 import signal
@@ -29,6 +24,8 @@ from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import Odometry
 from tf2_ros import TransformBroadcaster
 from std_msgs.msg import Int16
+from atlas_msgs.msg import DiffDriveMotorVelocities
+from atlas_msgs.msg import DiffDriveEncoderReadings
 
 NS_TO_SEC= 1000000000
 
@@ -59,36 +56,7 @@ class odometryNode(Node):
         super().__init__('odometry_node')
         self.name = "odometry"
         self.get_logger().info("Odometry Node Online!")
-        # GPIO Set Up
-        self.levLA = 0
-        self.levLB = 0
-        self.levRA = 0
-        self.levRB = 0
-
-        self.lastGpio = None
-        self.gpioLA = GPIO_LA
-        self.gpioLB = GPIO_LB
-        self.gpioRA = GPIO_RA
-        self.gpioRB = GPIO_RB
-
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.gpioLA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.gpioLB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.gpioRA, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.gpioRB, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-        GPIO.add_event_detect(self.gpioLA, GPIO.BOTH, self.Lencoder_callback)
-        GPIO.add_event_detect(self.gpioLB, GPIO.BOTH, self.Lencoder_callback)
-        GPIO.add_event_detect(self.gpioRA, GPIO.BOTH, self.Rencoder_callback)
-        GPIO.add_event_detect(self.gpioRB, GPIO.BOTH, self.Rencoder_callback)
-
-        # Encoder decoder set up
-        self.Lcounter = 0
-        self.Rcounter = 0
-        self.Llast_counter = 0
-        self.Rlast_counter = 0
-        self.last_publish_time = 0
-
+       
         # Odometry data set up
         self.x = 0.0  # position in xy plane
         self.y = 0.0
@@ -98,73 +66,29 @@ class odometryNode(Node):
         self.base_frame_id = self.declare_parameter('base_frame_id', 'base_link').value # robot's base frame
         self.odom_frame_id = self.declare_parameter('odom_frame_id', 'odom').value  # odometry reference frame
 
+        # Subscription set up
+        self.subscription = self.create_subscription(DiffDriveEncoderReadings,'encReading_perTick',self.periodicPublisher,10)
+
         # Publisher set up
-        self.publisher_ = self.create_publisher(Float32MultiArray, "odometry_state_vector_pertick", queue_size = 10)
+        self.publisher_ = self.create_publisher(Odometry, "odometry_pertick", queue_size = 10)
     
         # Publishing Timer
         self.timer = self.create_timer(1/TICK_RATE, self.periodicPublisher)
 
-    def Lencoder_callback(self, channel):
-        # Checking the most recent pin set to "high"
-        Llevel = GPIO.input(channel)
-        if channel == self.gpioLA:
-            self.levLA = Llevel
-        else:
-            self.levLB = Llevel
-      
-        # Debounce
-        if channel == self.lastGpio:
-            return
-    
-        # When both inputs are at 1, we'll fire a callback. If A was the most
-        # recent pin set high, it'll be forward, and if B was the most recent pin
-        # set high, it'll be reverse.
-        self.lastGpio = channel
-        if channel == self.gpioLA and Llevel == 1:    #Forward
-            if self.levLB == 1:
-                self.Lcounter += 1
-        elif channel == self.gpioLB and Llevel == 1:  #Reverse
-            if self.levLA == 1:
-                self.Lcounter -= 1
 
-
-    def Rencoder_callback(self, channel):
-        # Checking the most recent pin set to "high"
-        Rlevel = GPIO.input(channel)
-        if channel == self.gpioRA:
-            self.levRA = Rlevel
-        else:
-            self.levRB = Rlevel
-      
-        # Debounce
-        if channel == self.lastGpio:
-            return
-    
-        # When both inputs are at 1, we'll fire a callback. If A was the most
-        # recent pin set high, it'll be forward, and if B was the most recent pin
-        # set high, it'll be reverse.
-        self.lastGpio = channel
-        if channel == self.gpioRA and Rlevel == 1:    #Forward
-            if self.levRB == 1:
-                self.Rcounter += 1
-        elif channel == self.gpioRB and Rlevel == 1:  #Reverse
-            if self.levRA == 1:
-                self.Rcounter -= 1
-
-
-    def periodicPublisher(self):
+    def periodicPublisher(self, encReading_perTick):
         # Calculate elapsed time
         current_time = self.get_clock().now()
         time_elapsed = current_time - self.last_publish_time
-        time_elapsed = time_elapsed.nanoseconds / 1000000000
+        time_elapsed = time_elapsed.nanoseconds / NS_TO_SEC
         ticks_elapsed = time_elapsed/(1/TICK_RATE)
 
         # Calculate counts per tick
         if ticks_elapsed > 0:
-            Lcounts_perTick = (self.Lcounter - self.Llast_counter) / ticks_elapsed
-            Rcounts_perTick = (self.Rcounter - self.Rlast_counter) / ticks_elapsed
+            Lcounts_perTick = encReading_perTick.l_motor_encoder_val
+            Rcounts_perTick = encReading_perTick.r_motor_encoder_val
         else:
-            print("Tick period = 0")
+            print("Odometry: Time period = 0")
 
         # Convert counts/tick to m/tick
         Lw = Lcounts_perTick * (360/48) * (1/74.8317)       #Counts/Tick * Angle/Count * Exact Gear Ratio = Angle/Tick * Exact Gear Ratio (Degrees/Tick)
@@ -224,22 +148,14 @@ class odometryNode(Node):
         self.Llast_counter = self.Lcounter
         self.Rlast_counter = self.Rcounter
         self.last_publish_time = current_time    
-        
-
-    def destroy(self):
-        GPIO.remove_event_detect(self.gpioLA)
-        GPIO.remove_event_detect(self.gpioLB)
-        GPIO.remove_event_detect(self.gpioRA)
-        GPIO.remove_event_detect(self.gpioRB)
-        GPIO.cleanup()
-
+    
 
 class encoderError(Exception):
     print("Encoder Error Occurred. Bypassing Error")
     pass
 
 
-def main():
+def main(args=None):
     rclpy.init(args = args)
     try:
         odometry_node = odometryNode()
@@ -254,3 +170,6 @@ def main():
 if __name__ == "__main__":  
     main()
    
+
+
+
