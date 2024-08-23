067d2f91212ed8b17d5fdf5d4c5ebad9a73245d4
Hung Anh Dau <hdau0002@student.monash.edu>
2024-08-24 03:09:21 +1000

    Fixed unicode encoding error

--- atlas_ws/src/atlas_python/atlas_python/encoder.py
+++ atlas_ws/src/atlas_python/atlas_python/encoder.py
@@ -57,7 +57,7 @@ class Encoder(Node):
 
     def timer_callback(self):
         try:
-            self.serial.write('e')
+            self.serial.write(str.encode('e'))
             # Read from the serial port
             if self.serial.in_waiting > 0:
                 line = self.serial.readline().decode('utf-8').strip()
