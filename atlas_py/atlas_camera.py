0c0dcc789739b124c41fa581d35e3e5f62394201
Tyson Truong <tyson@dromeda.com.au>
2024-08-28 20:15:30 +1000

    new camera

--- atlas_py/atlas_camera.py
+++ atlas_py/atlas_camera.py
@@ -1,9 +1,7 @@
 import sys
 import cv2
 import logging
-
-# Camera params
-CAMERA_NUMBER = 0
+import numpy as np
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -13,46 +11,28 @@ class Camera():
     def __init__(
             self,
             camera_number: int = 0,
-            camera_width: int = 640,
-            camera_height: int = 480,
             ): 
         self.logger = logging.getLogger(__name__)
-        
-        self.camera_width = camera_width
-        self.camera_height = camera_height
 
-        self.cap = cv2.VideoCapture(camera_number)
-
-        if not self.cap.isOpened():
-            raise RuntimeError("Could not open camera")
+        self.camera_number = camera_number
         
         self.logger.info("Camera initialised")
 
-    def capture_image(self):
+    def capture_image(self) -> np.ndarray | None:
         """Capture an image from the camera and return it"""
 
-        ret, image = self.cap.read()
-        if not ret:
-            raise RuntimeError("Could not capture an image from the camera")
+        cap = cv2.VideoCapture(self.camera_number)
+
+        if not cap.isOpened():
+            self.logger("Error: Could not open webcam.")
+            return
         
-        return cv2.resize(image, (self.camera_width, self.camera_height))
-
-    def cleanup(self) -> None:
-        """Clean up the rest of the resources"""
-        self.cap.release()
-
-
-# def take_image(args: dict = None):
-#     camera = Camera()
-#     try:
-#         ret, image = camera.capture_image()
-#     except:
-#         self.logger.info("camera.py:Camera failed")
-#         camera.cleanup()
-#         sys.exit(1)
-#     finally:
-#         camera.cleanup()
-
-#     return image
+        ret, frame = cap.read()
+        cap.release()
+        
+        if ret:
+            return frame
+        else:
+            return
 
 
