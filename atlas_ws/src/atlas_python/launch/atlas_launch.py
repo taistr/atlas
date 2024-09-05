9d1d61f11833c0add361aa2bb9a33515788cc3af
Tyson Truong <tystruong@gmail.com>
2024-09-05 13:38:01 +1000

    Remove commented out launch args

--- atlas_ws/src/atlas_python/launch/atlas_launch.py
+++ atlas_ws/src/atlas_python/launch/atlas_launch.py
@@ -3,16 +3,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(
-        #     package='camera_ros',
-        #     executable='camera_node',
-        # ),
+        Node(
+            package='camera_ros',
+            executable='camera_node',
+        ),
         Node(
             package='atlas_python',
             executable='hello',
         ),
-        # Node(
-        #     package='atlas_python',
-        #     executable='object_detection',
-        # ),
+        Node(
+            package='atlas_python',
+            executable='object_detection',
+        ),
     ])
