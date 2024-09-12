4df83d504584cd0ec74ac593dba76fc12fb94c90
Tyson Truong <tyson@dromeda.com.au>
2024-09-12 19:42:03 +1000

    Launch descriptions for oibject detection and camera

--- atlas_ws/src/atlas_python/launch/atlas_launch.py
+++ atlas_ws/src/atlas_python/launch/atlas_launch.py
@@ -1,21 +1,36 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch_ros.descriptions import ParameterValue
+from launch_ros.parameter_descriptions import Parameter
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            "model_name",
+            default_value="29_08_640px.onnx",
+            description="Name of the model to use for object detection",
+        ),
+        DeclareLaunchArgument(
+            "camera",
+            default_value=0,
+            description="Camera number to use for object detection",
+        ),
         Node(
             package='camera_ros',
             executable='camera_node',
             parameters=[
-                {   
-                    "camera": 1,
-                    "height": 480,
-                    "width": 640,
-                } 
+                Parameter("camera", LaunchConfiguration("camera")),
+                Parameter("height", 480),
+                Parameter("width", 640)
             ],
         ),
         Node(
             package='atlas_python',
             executable='object_detection',
+            parameters=[
+                Parameter("model_name", LaunchConfiguration("model_name"))
+            ],
         ),
     ])
