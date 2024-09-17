fa793e2670ecea122beb39ea1ec49af0bd2a2197
Hung Anh Dau <hdau0002@student.monash.edu>
2024-09-17 12:03:09 +1000

    ros2 serial_comms modified to handle new comms protocol

--- atlas_ws/src/atlas_python/atlas_python/serial_comms.py
+++ atlas_ws/src/atlas_python/atlas_python/serial_comms.py
@@ -1,4 +1,6 @@
 import serial
+import select
+import sys
 import time
 from time import sleep
 from threading import Lock
@@ -39,6 +41,14 @@ class duinoCmdStruct:
                 self.arg4 = float(parts[5])
         except ValueError:
             print(f"Error parsing serial data: {serial_data}")
+    
+    def reset(self):
+        self.status = 400
+        self.cmd = ''
+        self.arg1 = 0.0
+        self.arg2 = 0.0
+        self.arg3 = 0.0
+        self.arg4 = 0.0
             
 
 class test_motorrequest:
@@ -55,11 +65,19 @@ class SerialComms(Node):
 
         self.logger = logging.getLogger(__name__)
 
-        self.serial = serial.Serial(
-            port=self.get_parameter("port_name").value, 
-            baudrate=self.get_parameter("BAUD_RATE").value, 
-            timeout=1
-        )
+        # Flags
+        self.COMMAND_ACCEPTED = False
+        self.COMMAND_COMPLETE = False
+        self.SENT_FLAG = False
+        self.DEBUG = False
+
+        if not self.DEBUG:
+            self.serial = serial.Serial(
+                port=self.get_parameter("port_name").value, 
+                baudrate=self.get_parameter("BAUD_RATE").value, 
+                timeout=1
+            )
+
         serial_open_time = self.get_clock().now().nanoseconds
 
         # 2 second delay
@@ -73,22 +91,10 @@ class SerialComms(Node):
         self.serialRxStruct = duinoCmdStruct()
         self.serialTxStruct = duinoCmdStruct()
 
-        # Flags
-        self.COMMAND_COMPLETE = False
-        self.SENT_FLAG = False
-        self.DEBUG = True
-
         # Setting up service
         self.srv = self.create_service(MotionRequest, 'motion_request', self.send_command)
 
         self.get_logger().info("Serial Comms initialised")
-
-        if self.DEBUG:
-            testStruct = test_motorrequest()
-            testStruct.angle = 0.0
-            testStruct.distance = 0.0
-            response = self.send_command(testStruct, "Complete")
-            print(response)
 
     # Initialises parameters
     def initialise_parameters(self) -> None:
@@ -117,13 +123,16 @@ class SerialComms(Node):
         self.serialTxStruct.arg3 = 0.0
         self.serialTxStruct.arg4 = 0.0
         self.serialTxStruct.status = 100
-        self.serialTxStruct.cmd = 'm'
+        self.serialTxStruct.cmd = request.CMD
         # Send data over serial
         self.mutex_serial.acquire()
         try:
             print("sending")
             send_str = self.serialTxStruct.to_serial_format() + "\r"
-            self.serial.write(send_str.encode('utf-8'))
+            
+            if not self.DEBUG:
+                self.serial.write(send_str.encode('utf-8'))
+
             send_time = self.get_clock().now().nanoseconds
             print(f"Sent: {send_str.strip()}")
             
@@ -133,22 +142,52 @@ class SerialComms(Node):
                 pass
 
             while not self.COMMAND_COMPLETE:
-                if self.serial.in_waiting <=0:
-                    current_time = self.get_clock().now().nanoseconds
-                    if current_time > send_time + 4000000000:
-                        raise Exception("Serial Timed Out.")
-                elif self.serial.in_waiting > 0:
-                    incoming_data = self.serial.readline().decode('utf-8')
-                    print(f"Received raw data: {incoming_data.strip()}")
-                    self.serialRxStruct.from_serial_format(incoming_data)
-                    print('RCVD:', self.serialRxStruct.status, self.serialRxStruct.cmd, self.serialRxStruct.arg1, self.serialRxStruct.arg2, self.serialRxStruct.arg3, self.serialRxStruct.arg4)
+                if not self.DEBUG:
+                    if self.serial.in_waiting <=0:
+                        current_time = self.get_clock().now().nanoseconds
+                        if self.COMMAND_ACCEPTED:
+                            timeout_period = 15000000000    #nanoseconds
+                        else:
+                            timeout_period = 4000000000     #nanoseconds
+                        if current_time > send_time + timeout_period:
+                            raise Exception("Serial Timed Out.")
+                    elif self.serial.in_waiting > 0:
+                        incoming_data = self.serial.readline().decode('utf-8')
+                        print(f"Received raw data: {incoming_data.strip()}")
+                        self.serialRxStruct.from_serial_format(incoming_data)
+                        print('RCVD:', self.serialRxStruct.status, self.serialRxStruct.cmd, self.serialRxStruct.arg1, self.serialRxStruct.arg2, self.serialRxStruct.arg3, self.serialRxStruct.arg4)
+
+                ###### DEBUG BLOCK - Activated by self.DEBUG flag
+                if self.DEBUG:
+                    if self.COMMAND_ACCEPTED:
+                            timeout_period = 15000000000    #nanoseconds
+                    else:
+                            timeout_period = 4000000000     #nanoseconds
+
+                    a_debug, b_debug, c_debug = select.select([sys.stdin], [], [], timeout_period/1000000000) 
+                    # Run if statement till the time is running 
+                    if (a_debug): 
+                        # Read the input and print result 
+                        incoming_data_debug = sys.stdin.readline().strip()
+                        print(f"Received raw data: {incoming_data_debug.strip()}")
+                        self.serialRxStruct.from_serial_format(incoming_data_debug)
+                        print('RCVD:', self.serialRxStruct.status, self.serialRxStruct.cmd, self.serialRxStruct.arg1, self.serialRxStruct.arg2, self.serialRxStruct.arg3, self.serialRxStruct.arg4)
+                    else:
+                        print("Emulated Serial timed out")
+                ##############################################
 
                 if self.serialRxStruct.status == 200:
                     self.COMMAND_COMPLETE = True
+                    self.serialRxStruct.reset()
                     return response
                 elif self.serialRxStruct.status == 202:
                     send_time = self.get_clock().now().nanoseconds
+                    self.COMMAND_ACCEPTED = True
+                    self.serialRxStruct.reset()
                 else:
+                    self.COMMAND_ACCEPTED = False
+                    self.COMMAND_COMPLETE = False
+                    self.serialRxStruct.reset()
                     pass
                     
         except Exception as e:
