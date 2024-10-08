937f8f2874303f748f101cb43573af6f272abad0
Tyson Truong <tystruong@gmail.com>
2024-10-08 22:10:37 +1100

    limit switch test script

---
+++ atlas_py/atlas_py/test_limit_switches.py
@@ -0,0 +1,28 @@
+import time
+import RPi.GPIO as GPIO
+from limit_switches import LimitSwitches  # Replace 'your_module' with the actual module name
+
+# Create an instance of the LimitSwitches class
+limit_switches = LimitSwitches()
+
+try:
+    print("Press the limit switches to test...")
+    while True:
+        # Check if any limit switch is pressed
+        if limit_switches.switches_pressed():
+            if limit_switches.left_limit_switch_pressed():
+                print("Left limit switch pressed!")
+            if limit_switches.right_limit_switch_pressed():
+                print("Right limit switch pressed!")
+        else:
+            print("No limit switches pressed.")
+
+        time.sleep(0.1)  # Small delay to avoid rapid looping
+
+except KeyboardInterrupt:
+    print("Test interrupted by the user.")
+
+finally:
+    # Clean up GPIO settings before exiting
+    limit_switches.cleanup()
+    print("GPIO cleanup completed.")
