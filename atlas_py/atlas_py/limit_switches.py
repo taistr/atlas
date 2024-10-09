a6bb22ae4cd55016bb77758c2bb6134d21c233f3
Hung Anh Dau <hdau0002@student.monash.edu>
2024-10-10 08:48:50 +1100

    limit_switches.py can return pressed switches

--- atlas_py/atlas_py/limit_switches.py
+++ atlas_py/atlas_py/limit_switches.py
@@ -10,9 +10,22 @@ class LimitSwitches:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(LEFT_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(RIGHT_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(LEFTLOWER_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(RIGHTLOWER_LIMIT_SWITCH_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def switches_pressed(self):
-        return not GPIO.input(LEFT_LIMIT_SWITCH_PIN) or not GPIO.input(RIGHT_LIMIT_SWITCH_PIN)
+        pressed_switches = []
+
+        if not GPIO.input(LEFT_LIMIT_SWITCH_PIN):
+            pressed_switches.append(LEFT_LIMIT_SWITCH_PIN)
+        if not GPIO.input(RIGHT_LIMIT_SWITCH_PIN):
+            pressed_switches.append(RIGHT_LIMIT_SWITCH_PIN)
+        if not GPIO.input(LEFTLOWER_LIMIT_SWITCH_PIN):
+            pressed_switches.append(LEFTLOWER_LIMIT_SWITCH_PIN)
+        if not GPIO.input(RIGHTLOWER_LIMIT_SWITCH_PIN):
+            pressed_switches.append(RIGHTLOWER_LIMIT_SWITCH_PIN)
+
+        return pressed_switches  # Return the array directly
 
     def cleanup(self):
         GPIO.cleanup()
